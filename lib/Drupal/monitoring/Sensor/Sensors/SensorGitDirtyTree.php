f9a941c2c3653a52d602cc6531b1ac633fd3d0ca
git <git@1275178.no-reply.drupal.org>
2015-05-20 17:56:08 +0200

    Issue #2463769 by chaos6522, Berdir, miro_dietiker: Extend sensor: Git: show dirty file in summary message

--- lib/Drupal/monitoring/Sensor/Sensors/SensorGitDirtyTree.php
+++ lib/Drupal/monitoring/Sensor/Sensors/SensorGitDirtyTree.php
@@ -38,7 +38,7 @@ class SensorGitDirtyTree extends SensorConfigurable implements SensorExtendedInf
 
     if (!empty($this->cmdOutput)) {
       $result->setValue(count(explode("\n", $this->cmdOutput)));
-      $result->addStatusMessage('Files in unexpected state');
+      $result->addStatusMessage('Files in unexpected state: ' . $this->getShortFileList($this->cmdOutput, 2));
       $result->setStatus(SensorResultInterface::STATUS_CRITICAL);
     }
     else {
@@ -67,5 +67,41 @@ class SensorGitDirtyTree extends SensorConfigurable implements SensorExtendedInf
     $repo_path = DRUPAL_ROOT . '/' . $this->info->getSetting('repo_path');
     $cmd = $this->info->getSetting('cmd');
     return 'cd ' . escapeshellarg($repo_path) . "\n$cmd  2>&1";
+  }
+
+  /**
+   * Returns a shortened file list for the status message.
+   *
+   * @param string $input
+   *   Result from running the git command.
+   *
+   * @param int $max_files
+   *   Limit the number of files returned.
+   *
+   * @param int $max_length
+   *   Limit the length of the path to the file
+   *
+   * @return string
+   *   File names from $output.
+   */
+  protected function getShortFileList($input, $max_files = 2, $max_length = 50) {
+    $output = array();
+    // Remove unnecessary whitespace.
+    $input = preg_replace('/\s\s+/', ' ', trim($input));
+    $lines = explode(PHP_EOL, $input);
+    foreach (array_slice($lines, 0, $max_files) as $line) {
+      // Separate type of modification and path to file.
+      $parts = explode(' ', $line, 2);
+      if (strlen($parts[1]) > $max_length) {
+        // Put together type of modification and path to file limited by
+        // $pathLength.
+        $output[] = $parts[0] . ' …' . substr($parts[1], -$max_length);
+      }
+      else {
+        // Return whole line if path is shorter then $pathLength.
+        $output[] = $line;
+      }
+    }
+    return implode(', ', $output);
   }
 }
