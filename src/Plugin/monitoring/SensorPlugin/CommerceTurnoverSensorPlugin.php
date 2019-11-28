392aa2985e4f07c9e3b7284815181317fb9f6049
arpad.rozsa <arpad.rozsa@3509607.no-reply.drupal.org>
2019-11-28 16:27:29 +0100

    Issue #3086728 by arpad.rozsa: Make commerce turnover sensor work in D8

--- src/Plugin/monitoring/SensorPlugin/CommerceTurnoverSensorPlugin.php
+++ src/Plugin/monitoring/SensorPlugin/CommerceTurnoverSensorPlugin.php
@@ -1,14 +1,17 @@
 <?php
-/**
- * @file
- * Contains \Drupal\monitoring\Plugin\monitoring\SensorPlugin\CommerceTurnoverSensorPlugin.
- */
 
 namespace Drupal\monitoring\Plugin\monitoring\SensorPlugin;
 
-use Drupal\Core\Database\Query\SelectInterface;
+use CommerceGuys\Intl\Formatter\CurrencyFormatterInterface;
+use Drupal\commerce_price\Entity\CurrencyInterface;
+use Drupal\commerce_store\CurrentStoreInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\monitoring\Entity\SensorConfig;
 use Drupal\monitoring\Result\SensorResultInterface;
+use Drupal\state_machine\WorkflowManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Monitors commerce order turnover stats.
@@ -20,11 +23,58 @@ use Drupal\monitoring\Result\SensorResultInterface;
  *   label = @Translation("Commerce order turnover"),
  *   description = @Translation("Monitors how much money was earned with commerce orders."),
  *   provider = "commerce_order",
- *   addable = FALSE
+ *   addable = TRUE
  * )
- *
  */
 class CommerceTurnoverSensorPlugin extends ContentEntityAggregatorSensorPlugin {
+
+  /**
+   * The commerce currency formatter.
+   *
+   * @var \CommerceGuys\Intl\Formatter\CurrencyFormatterInterface
+   */
+  protected $currencyFormatter;
+
+  /**
+   * The current store service.
+   *
+   * @var \Drupal\commerce_store\CurrentStoreInterface
+   */
+  protected $currentStore;
+
+  /**
+   * The workflow manager.
+   *
+   * @var \Drupal\state_machine\WorkflowManagerInterface
+   */
+  protected $workflowManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(SensorConfig $sensor_config, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, CurrencyFormatterInterface $currency_formatter, CurrentStoreInterface $current_store, WorkflowManagerInterface $workflow_manager) {
+    parent::__construct($sensor_config, $plugin_id, $plugin_definition, $entity_type_manager, $entity_field_manager);
+
+    $this->currencyFormatter = $currency_formatter;
+    $this->currentStore = $current_store;
+    $this->workflowManager = $workflow_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, SensorConfig $sensor_config, $plugin_id, $plugin_definition) {
+    return new static(
+      $sensor_config,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager'),
+      $container->get('commerce_price.currency_formatter'),
+      $container->get('commerce_store.current_store'),
+      $container->get('plugin.manager.workflow')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -32,20 +82,11 @@ class CommerceTurnoverSensorPlugin extends ContentEntityAggregatorSensorPlugin {
   protected function getEntityQueryAggregate() {
     $query = parent::getEntityQueryAggregate();
 
-    // Get the field name for the amount field.
-    $field_amount = $this->getFieldName($query, array('field' => 'commerce_order_total.amount'));
+    $query->aggregate('total_price.number', 'SUM');
+    $query->condition('total_price.currency_code', $this->sensorConfig->getSetting('commerce_order_currency'));
 
-    // Build the field name based on that for the currency in the same table.
-    list($alias) = explode('.', $field_amount);
-    $field_currency = _field_sql_storage_columnname('commerce_order_total', 'currency_code');
-
-    // Get the amount sum.
-    $query->addExpression('sum(' . $field_amount . ')', 'sum_amount');
-    $query->addField($alias, $field_currency, 'currency_code');
-    $query->groupBy($alias . '.' . $field_currency);
-
-    if ($currency_code = $this->sensorConfig->getSetting('currency_code')) {
-      $query->condition($alias . '.' . $field_currency, $currency_code);
+    if ($paid_states = array_filter($this->sensorConfig->getSetting('commerce_order_paid_states'))) {
+      $query->condition('state', $paid_states, 'IN');
     }
 
     return $query;
@@ -55,49 +96,81 @@ class CommerceTurnoverSensorPlugin extends ContentEntityAggregatorSensorPlugin {
    * {@inheritdoc}
    */
   public function runSensor(SensorResultInterface $result) {
+    parent::runSensor($result);
+
+    $query_result = $this->getEntityQueryAggregate()->execute();
+    $currency_code = $this->sensorConfig->getSetting('commerce_order_currency');
     $sensor_value = 0;
-    $currency_code = $this->sensorConfig->getSetting('currency_code');
-    foreach ($this->getQueryResult()->fetchAll() as $row) {
-      // In case the sensor defines currency_code in settings and it is equal
-      // to the currency_code from the db result the sensor value is sum of
-      // amounts for that currency_code.
-      if ($currency_code == $row->currency_code) {
-        $sensor_value = $row->sum_amount;
-        break;
-      }
-      elseif ($row->currency_code != commerce_default_currency()) {
-        $sensor_value += commerce_currency_convert($row->sum_amount, $row->currency_code, commerce_default_currency());
-      }
-      else {
-        $sensor_value += $row->sum_amount;
-      }
+
+    if (!empty($query_result)) {
+      $query_result = reset($query_result);
+
+      $sensor_value = $query_result['total_pricenumber_sum'];
     }
 
-    // Convert the amount into a decimal for either the configured currency
-    // or the default if none is configured.
-    $result->setValue(commerce_currency_amount_to_decimal($sensor_value, $currency_code ? $currency_code : commerce_default_currency()));
+    $result->setValue($this->currencyFormatter->format($sensor_value, $currency_code));
   }
 
   /**
-   * Adds the order statuses select element to the sensor settings form.
+   * Adds additional settings to the sensor configuration form.
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
-    $conditions = $this->sensorConfig->getSetting('conditions');
 
-    $options = array();
-    foreach (commerce_order_statuses() as $name => $info) {
-      $options[$name] = $info['title'];
+    $form['commerce_order_paid_states'] = [
+      '#type' => 'checkboxes',
+      '#title' => t('"Paid" order states'),
+      '#description' => t('Select order states in which the order is considered to be paid.'),
+      '#options' => $this->getOrderStates(),
+      '#default_value' => $this->sensorConfig->getSetting('commerce_order_paid_states'),
+    ];
+
+    $currencies = $this->entityTypeManager->getStorage('commerce_currency')
+      ->loadMultiple();
+
+    $current_store = $this->currentStore->getStore();
+    $default_currency = $current_store->getDefaultCurrency();
+
+    foreach ($currencies as $currency_code => $currency) {
+      $currencies[$currency_code] = $currency->getName();
     }
-    $form['conditions']['status']['value'] = array(
+
+    $selected_currency = $this->sensorConfig->getSetting('commerce_order_currency') ?: '';
+
+    if (empty($selected_currency) && $default_currency instanceof CurrencyInterface) {
+      $selected_currency = $default_currency->getCurrencyCode();
+    }
+
+    $form['commerce_order_currency'] = [
       '#type' => 'select',
-      '#title' => t('"Paid" order statuses'),
-      '#description' => t('Select order statuses in which the order is considered to be paid.'),
-      '#options' => $options,
-      '#multiple' => TRUE,
-      '#default_value' => $conditions['status']['value'],
-    );
+      '#title' => t('Currency'),
+      '#description' => t('Select which currency the orders are using.'),
+      '#options' => $currencies,
+      '#default_value' => $selected_currency,
+      '#required' => TRUE,
+    ];
 
     return $form;
   }
+
+  /**
+   * Gets all order states from all workflows.
+   *
+   * @return array
+   *   An array of order states or empty array if no states found.
+   */
+  protected function getOrderStates() {
+    $states = [];
+
+    foreach ($this->workflowManager->getDefinitions() as $workflow) {
+      if ($workflow['group'] == 'commerce_order') {
+        foreach ($workflow['states'] as $key => $state) {
+          $states[$key] = $state['label'];
+        }
+      }
+    }
+
+    return $states;
+  }
+
 }
---
+++ tests/src/Functional/MonitoringCommerceTest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Drupal\Tests\monitoring\Functional;
+
+use Drupal\commerce_order\Entity\Order;
+use Drupal\commerce_order\Entity\OrderItem;
+use Drupal\commerce_price\Price;
+use Drupal\monitoring\Entity\SensorConfig;
+
+/**
+ * Tests the commerce turnover sensor.
+ *
+ * @group monitoring
+ */
+class MonitoringCommerceTest extends MonitoringTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  public static $modules = ['commerce', 'commerce_order', 'node'];
+
+  /**
+   * The account for testing.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $account;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->account = $this->drupalCreateUser([
+      'administer commerce_order',
+      'administer commerce_currency',
+      'administer commerce_store',
+      'administer commerce_order_type',
+      'administer monitoring',
+      'monitoring reports',
+    ]);
+    $this->drupalLogin($this->account);
+
+    // Enable currencies.
+    $edit = [
+      'currency_codes[]' => ['CHF', 'EUR'],
+    ];
+    $this->drupalPostForm('admin/commerce/config/currencies/add', $edit, t('Add'));
+
+    // Create a default store.
+    $edit = [
+      'name[0][value]' => 'Default',
+      'mail[0][value]' => 'test@example.com',
+      'address[0][address][country_code]' => 'CH',
+      'address[0][address][address_line1]' => 'Demo street',
+      'address[0][address][locality]' => 'Demo city',
+      'address[0][address][postal_code]' => '1234',
+      'default_currency' => 'CHF',
+    ];
+    $this->drupalPostForm('store/add/online', $edit, t('Save'));
+
+    // Create an order item type
+    $edit = [
+      'id' => 'test',
+      'label' => 'Test',
+      'orderType' => 'default'
+    ];
+    $this->drupalPostForm('admin/commerce/config/order-item-types/add', $edit, t('Save'));
+  }
+
+  /**
+   * Tests the commerce turnover sensor.
+   */
+  public function testTurnoverSensor() {
+    // Create commerce turnover sensor.
+    $sensor = SensorConfig::create([
+      'id' => 'commerce_total_turnover',
+      'label' => 'Total turnover',
+      'plugin_id' => 'commerce_turnover',
+      'status' => 1,
+      'settings' => [
+        'entity_type' => 'commerce_order',
+        'commerce_order_paid_states' => [],
+        'commerce_order_currency' => 'CHF',
+        'time_interval_field' => 'created',
+        'time_interval_value' => 86400,
+      ],
+    ]);
+    $sensor->save();
+
+    // Create some orders with different states and currencies.
+    $this->createEmptyOrderWithPrice('canceled');
+    $this->createEmptyOrderWithPrice('draft');
+    $this->createEmptyOrderWithPrice('draft', 200, 'CHF');
+    $this->createEmptyOrderWithPrice('draft', 1000, 'EUR');
+    $this->createEmptyOrderWithPrice('completed', 1500, 'CHF');
+
+    $result = $this->runSensor('commerce_total_turnover');
+    $this->assertEqual($result->getMessage(), 'Value CHF 1’900.00 in 1 day');
+
+    // Now only consider completed orders.
+    $edit = [
+      'settings[commerce_order_paid_states][completed]' => TRUE,
+    ];
+    $this->drupalPostForm('admin/config/system/monitoring/sensors/commerce_total_turnover', $edit, t('Save'));
+
+    $sensor = SensorConfig::load('commerce_total_turnover');
+    $paid_states = $sensor->getSetting('commerce_order_paid_states');
+    $this->assertNotContains('draft', $paid_states);
+    $this->assertContains('completed', $paid_states);
+
+    $result = $this->runSensor('commerce_total_turnover');
+    $this->assertEqual($result->getMessage(), 'Value CHF 1’500.00 in 1 day');
+
+    // Change currency.
+    $this->createEmptyOrderWithPrice('completed', 250, 'EUR');
+    $edit = [
+      'settings[commerce_order_currency]' => 'EUR',
+    ];
+    $this->drupalPostForm('admin/config/system/monitoring/sensors/commerce_total_turnover', $edit, t('Save'));
+
+    $result = $this->runSensor('commerce_total_turnover');
+    $this->assertEqual($result->getMessage(), 'Value € 250.00 in 1 day');
+  }
+
+  /**
+   * Create an order for testing purposes.
+   *
+   * @param string $state
+   *   State in which the order should be created.
+   * @param mixed $amount
+   *   The total price of the order.
+   * @param string $currency
+   *   Currency of the total price.
+   */
+  protected function createEmptyOrderWithPrice($state, $amount = 100, $currency = 'CHF') {
+    $order_item = OrderItem::create([
+      'type' => 'test',
+      'quantity' => 1,
+      'unit_price' => new Price((string) $amount, $currency),
+      'overridden_unit_price' => TRUE,
+    ]);
+    $order_item->save();
+
+    $order = Order::create([
+      'type' => 'default',
+      'state' => $state,
+      'store_id' => \Drupal::service('commerce_store.current_store')->getStore()->id(),
+      'order_items' => [$order_item],
+    ]);
+    $order->save();
+  }
+
+}
